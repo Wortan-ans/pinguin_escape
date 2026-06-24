b6d5c3e757b974056b6ad4e94c9ecf9f17c2e247
Wortan-ans <andre.nsc.eng@gmail.com>
2026-06-24 17:42:37 -0300

    criação do loop de janela

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from code.game import Game
 
-print("Starting...")
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print("Setup End")
-
-#loop para manter a janela aberta e fecha-la.
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
